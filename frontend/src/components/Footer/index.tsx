613fd9b809343acb0869f459a73b8eaefd0337b6
André Luis <andrelluis17@hotmail.com>
2021-10-05 18:43:35 -0300

    Componentes estaticos basicos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+      <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/sirAndre1337" target="_blank" rel="noreferrer">André Lacerda</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela DevSuperior</small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
