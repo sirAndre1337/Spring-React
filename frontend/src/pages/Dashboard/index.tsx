72537d4d83aa0a5999f2eb28f740f0ddbde95d57
André Luis <andrelluis17@hotmail.com>
2021-10-07 16:28:57 -0300

    Primeira requisiçao no backend

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -6,8 +6,7 @@ import DonutChart from "components/DonutChart";
 
 const Dashboard = () => {
     return (
-        <div>
-            <>
+      <>
       <NavBar />
     <div className="container">
       <h1 className="text-primary py-3">Dashboard de Vendas</h1>
@@ -18,7 +17,7 @@ const Dashboard = () => {
           <BarChart />
         </div>
         <div className="col-sm-6">
-        <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+        <h5 className="text-center text-secondary">Total de vendas (%)</h5>
           <DonutChart />
         </div>
       </div>
@@ -31,7 +30,7 @@ const Dashboard = () => {
     </div>
       <Footer />
     </>
-        </div>
+        
     );
 }
 
